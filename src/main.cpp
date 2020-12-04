cf8239cfcb546ab0c867f42baa7afd3a101f6c40
Calin <ardelean.calin@outlook.com>
2020-12-04 11:10:29 +0200

    Added Homekit support

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,39 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
+#include <PubSubClient.h>
 #include <SPIFFS.h>
-#include <WebSocketsServer.h>
+#include <TelnetStream.h>
 #include <WiFi.h>
+#include <math.h>
 
-// Function prototypes
-void processWebsocketData(uint8_t num, WStype_t type, uint8_t *payload,
-                          size_t length);
-void setRGB(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness);
+typedef struct RGBColor
+{
+  int r;
+  int g;
+  int b;
+} RGBColor;
+
+void setRGB(uint16_t r, uint16_t g, uint16_t b);
 String getContentType(String filename);
 bool handleFileRead(String path, AsyncWebServerRequest *request);
+RGBColor hsv2rgb(float H, float S, float V);
+void mqttCallback(char *topic, byte *payload, unsigned int length);
+void reconnectMqtt();
 
 // Replace with your network credentials
 const char *ssid = "Calin-Network-2.4";
 const char *password = "Trappist-1";
 const char *hostname = "Lightstrip";
 
+// MQTT configuration
+const char *mqttServer = "192.168.1.243";
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
 // Set web server port number to 80
-AsyncWebServer server(80);
-// And a websocket server on port 81
-WebSocketsServer ws_server(81);
+// AsyncWebServer server(80);
 
 // Red, green, and blue pins for PWM control
 #define PIN_RED   23 // 23 corresponds to GPIO23
@@ -43,7 +56,19 @@ WebSocketsServer ws_server(81);
 #define LED_RESOLUTION     (12U)
 #define LED_MAX_BRIGHTNESS ((1 << LED_RESOLUTION) - 1)
 
+#define H_MAX 360
+#define S_MAX 100
+#define V_MAX 100
+
+#define RGB_UPDATE_FREQ           50
+#define RGB_UPDATE_PERIOD         ((float)(1000.0 / RGB_UPDATE_FREQ))
+#define RGB_ANIMATION_DURATION_MS 10000
+
 uint8_t resetCount = 0;
+float brightness = 0;
+float h = 0.0, s = 100.0, v = brightness;
+uint32_t lastCallTime = 0;
+uint32_t currCallTime = 0;
 
 void setup()
 {
@@ -69,17 +94,20 @@ void setup()
     delay(500);
     Serial.print(".");
     resetCount++;
-    if (resetCount >= 2)
+    if (resetCount >= 10)
     {
       ESP.restart();
     }
   }
 
   // Main webpage with LED control
-  server.onNotFound([](AsyncWebServerRequest *request) {   // If the client requests any URI
-    if (!handleFileRead(request->url(), request))          // send it if it exists
-      request->send(404, "text/plain", "404: Not Found");  // otherwise, respond with a 404 (Not Found) error
-  });
+  // server.onNotFound([](AsyncWebServerRequest *request) {   // If the client
+  // requests any URI
+  //   if (!handleFileRead(request->url(), request))          // send it if it
+  //   exists
+  //     request->send(404, "text/plain", "404: Not Found");  // otherwise,
+  //     respond with a 404 (Not Found) error
+  // });
 
   ArduinoOTA.onEnd([]() {
     // On upload end => restart ESP
@@ -89,96 +117,170 @@ void setup()
   ArduinoOTA.begin();
   // Hostname needs to be set after OTA
   MDNS.begin(hostname);
-  server.begin();
-  ws_server.begin(); // Start listening on port 81
-  ws_server.onEvent(processWebsocketData);
+  // server.begin();
+
+  mqttClient.setServer(mqttServer, 1883);
+  mqttClient.setCallback(mqttCallback);
+
+  TelnetStream.begin();
+
   // Default off
-  setRGB(0, 0, 0, 0);
+  setRGB(0, 0, 0);
 }
 
 void loop()
 {
   ArduinoOTA.handle();
-  ws_server.loop();
+  if (!mqttClient.connected())
+  {
+    reconnectMqtt();
+  }
+  else
+  {
+    mqttClient.loop();
+  }
+
+  /* Runs every few milliseconds to take the RGB strip through all colors of the
+   * rainbow */
+  currCallTime = millis();
+  if (currCallTime - lastCallTime > RGB_UPDATE_PERIOD)
+  { // 50Hz
+    // Periodic update of RGB LED value
+    h += H_MAX / (RGB_ANIMATION_DURATION_MS / RGB_UPDATE_PERIOD);
+    RGBColor xRGB = hsv2rgb(h, s, v);
+    setRGB(xRGB.r, xRGB.g, xRGB.b);
+    lastCallTime = currCallTime;
+  }
 }
 
-void processWebsocketData(uint8_t num, WStype_t type, uint8_t *payload,
-                          size_t length)
+void reconnectMqtt()
 {
-  switch (type)
+  uint8_t tries = 0;
+  // Loop until we're reconnected, but only try 5 times
+  while (!mqttClient.connected() && (tries < 5))
   {
-  case WStype_DISCONNECTED:
-    break;
-  case WStype_CONNECTED:
-  {
-    // send message to client
-    ws_server.sendTXT(num, "WebSocket Connected!");
-  }
-  break;
-  case WStype_TEXT:
-  {
-    char *pEnd;
-    uint8_t r = strtol((const char *)payload, &pEnd, 16);
-    uint8_t g = strtol(pEnd, &pEnd, 16);
-    uint8_t b = strtol(pEnd, &pEnd, 16);
-    uint8_t l = strtol(pEnd, &pEnd, 16);
-    setRGB(r, g, b, l);
-    break;
-  }
-  case WStype_BIN:
-  {
-    // webSocket.sendBIN(num, payload, length);
-    break;
-  }
-  case WStype_ERROR:
-  case WStype_FRAGMENT_TEXT_START:
-  case WStype_FRAGMENT_BIN_START:
-  case WStype_FRAGMENT:
-  case WStype_FRAGMENT_FIN:
-  case WStype_PING:
-  case WStype_PONG:
-    break;
-    break;
-    break;
+    // Attempt to connect
+    if (mqttClient.connect("ESP32Client"))
+    {
+      // Subscribe
+      mqttClient.subscribe("rgbon");
+      mqttClient.subscribe("rgbbrightness");
+    }
+    else
+    {
+      // Wait 5 seconds before retrying
+    }
+    tries++;
   }
 }
 
-String getContentType(String filename){
-  if(filename.endsWith(".html")) return "text/html";
-  else if(filename.endsWith(".css")) return "text/css";
-  else if(filename.endsWith(".js")) return "application/javascript";
-  else if(filename.endsWith(".ico")) return "image/x-icon";
-  else if(filename.endsWith(".gz")) return "application/x-gzip";
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  byte *data = (byte *)malloc(length * sizeof(byte) + 1);
+  memcpy(data, payload, length);
+  data[length] = '\0';
+
+  if (String(topic) == "rgbon")
+  {
+    TelnetStream.println("rgbon");
+    mqttClient.publish("rgbonStatus", data, length);
+    if (data[0] == '0')
+      v = 0.0;
+    else if (data[0] == '1')
+      v = brightness;
+  }
+  else if (String(topic) == "rgbbrightness")
+  {
+    mqttClient.publish("rgbbrightnessStatus", data, length);
+    brightness = String((char *)data).toFloat();
+    v = brightness;
+  }
+
+  free(data);
+}
+
+void setRGB(uint16_t r, uint16_t g, uint16_t b)
+{
+  ledcWrite(CHANNEL_RED, r);
+  ledcWrite(CHANNEL_GREEN, g);
+  ledcWrite(CHANNEL_BLUE, b);
+}
+
+RGBColor hsv2rgb(float H, float S, float V)
+{
+  float r, g, b;
+
+  float h = H / H_MAX;
+  float s = S / S_MAX;
+  float v = V / V_MAX;
+
+  int i = floor(h * 6);
+  float f = h * 6 - i;
+  float p = v * (1 - s);
+  float q = v * (1 - f * s);
+  float t = v * (1 - (1 - f) * s);
+
+  switch (i % 6)
+  {
+  case 0:
+    r = v, g = t, b = p;
+    break;
+  case 1:
+    r = q, g = v, b = p;
+    break;
+  case 2:
+    r = p, g = v, b = t;
+    break;
+  case 3:
+    r = p, g = q, b = v;
+    break;
+  case 4:
+    r = t, g = p, b = v;
+    break;
+  case 5:
+    r = v, g = p, b = q;
+    break;
+  }
+
+  RGBColor color;
+  color.r = r * LED_MAX_BRIGHTNESS;
+  color.g = g * LED_MAX_BRIGHTNESS;
+  color.b = b * LED_MAX_BRIGHTNESS;
+
+  return color;
+}
+
+String getContentType(String filename)
+{
+  if (filename.endsWith(".html"))
+    return "text/html";
+  else if (filename.endsWith(".css"))
+    return "text/css";
+  else if (filename.endsWith(".js"))
+    return "application/javascript";
+  else if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  else if (filename.endsWith(".gz"))
+    return "application/x-gzip";
   return "text/plain";
 }
 
-bool handleFileRead(String path, AsyncWebServerRequest *request){  // send the right file to the client (if it exists)
-  if(path.endsWith("/")) path += "index.html";           // If a folder is requested, send the index file
-  String contentType = getContentType(path);             // Get the MIME type
+bool handleFileRead(String path, AsyncWebServerRequest *request)
+{ // send the right file to the client (if it exists)
+  if (path.endsWith("/"))
+    path += "index.html"; // If a folder is requested, send the index file
+  String contentType = getContentType(path); // Get the MIME type
   String pathWithGz = path + ".gz";
-  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){  // If the file exists, either as a compressed archive, or normal
-    if(SPIFFS.exists(pathWithGz))                          // If there's a compressed version available
-      path += ".gz";    
-    AsyncWebServerResponse *response = request->beginResponse(SPIFFS, path, "text/html", false)   ;        // Send the file to the client
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+  { // If the file exists, either as a compressed archive, or normal
+    if (SPIFFS.exists(pathWithGz)) // If there's a compressed version available
+      path += ".gz";
+    AsyncWebServerResponse *response = request->beginResponse(
+        SPIFFS, path, "text/html", false); // Send the file to the client
     if (SPIFFS.exists(pathWithGz))
       response->addHeader("Content-Encoding", "gzip");
     request->send(response);
     return true;
   }
-  return false;                                          // If the file doesn't exist, return false
-}
-
-void setRGB(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness)
-{
-  // Brightness goes from 0 to 100;
-  uint16_t maxBrightness =
-      (uint16_t)map(brightness, 0, 100, 0, LED_MAX_BRIGHTNESS);
-  // Limit the duty cycle depending on brightness
-  uint16_t _r = (uint16_t)map(r, 0, 255, 0, maxBrightness);
-  uint16_t _g = (uint16_t)map(g, 0, 255, 0, maxBrightness);
-  uint16_t _b = (uint16_t)map(b, 0, 255, 0, maxBrightness);
-
-  ledcWrite(CHANNEL_RED, _r);
-  ledcWrite(CHANNEL_GREEN, _g);
-  ledcWrite(CHANNEL_BLUE, _b);
+  return false; // If the file doesn't exist, return false
 }
